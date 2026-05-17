8e065adde5fb41ea0c1242690ed1613cdfae7dcf
northlnwza <thanakrit.0127@gmail.com>
2026-05-18 00:17:13 +0700

    Rewrite resume compiler in Rust

---
+++ target/debug/deps/resume_compiler-68f70492a177fa16.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/win/Documents/Resume/target/debug/deps/resume_compiler-68f70492a177fa16.d: src/main.rs
+
+/mnt/c/Users/win/Documents/Resume/target/debug/deps/resume_compiler-68f70492a177fa16: src/main.rs
+
+src/main.rs:
